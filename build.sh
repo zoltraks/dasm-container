f0e25ef62fd107d15f1da646958b6677eef36e7f
Filip Golewski <f.golewski@gmail.com>
2025-10-20 01:57:57 +0200

    Use curl instead of wget if available

--- build.sh
+++ build.sh
@@ -18,7 +18,7 @@ mkdir -p "$CACHE"
 
 FILE="$CACHE/dasm-$VERSION-linux-x64.tar.gz"
 
-check_command() {
+require_command() {
     local cmd="$1"
     command -v "$cmd" >/dev/null 2>&1
     if [ $? -ne 0 ]; then
@@ -31,11 +31,29 @@ if [ -f "$FILE" ]
 then
   echo "File $FILE already exists"
 else
-  check_command wget
-  wget -P "$CACHE" "$URL" || exit 1
+  HAS_CURL=0
+  HAS_WGET=0
+  
+  command -v curl >/dev/null 2>&1 && HAS_CURL=1
+  command -v wget >/dev/null 2>&1 && HAS_WGET=1
+  
+  if [ $HAS_CURL -eq 0 ] && [ $HAS_WGET -eq 0 ]
+  then
+    echo "Error: Neither curl nor wget is installed"
+    exit 1
+  fi
+  
+  if [ $HAS_CURL -eq 1 ]
+  then
+    curl --version | head -n 1
+    curl -L -o "$FILE" "$URL" || exit 1
+  else
+    wget --version | head -n 1
+    wget -P "$CACHE" "$URL" || exit 1
+  fi
 fi
 
-check_command tar
+require_command tar
 
 if [ -d "$CACHE/dasm" ]
 then
@@ -46,9 +64,16 @@ mkdir -p "$CACHE/dasm"
 
 tar -xzf "$FILE" -C "$CACHE/dasm"
 
-check_command docker
+require_command docker
 
 docker buildx build --progress plain -t "$IMAGE" .
+
+if [ $? -ne 0 ]
+then
+    echo
+    echo "Error: Docker build failed"
+    exit 1
+fi
 
 echo
 echo "Now you can use docker command to compile your project using dasm"
